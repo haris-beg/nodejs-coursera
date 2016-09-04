b692ad6052e303725fd8b97bf9d959a49c6f9f24
Haris Beg <haris.beg@gmail.com>
2016-09-04 08:13:09 -0700

    Cosmetic code formatting changes

--- node-examples/solve-3.js
+++ node-examples/solve-3.js
@@ -9,15 +9,15 @@ function solveRect(l,b) {
     console.log("Solving for rectangle with l = "
                 + l + " and b = " + b);
     rect(l,b, function(err,rectangle) {
-	if (err) {
-	    console.log(err);
-	}
-	else {
+	     if (err) {
+	        console.log(err);
+	    }
+	    else {
             console.log("The area of a rectangle of dimensions length = "
                   + l + " and breadth = " + b + " is " + rectangle.area());
-	    console.log("The perimeter of a rectangle of dimensions length = "
+	          console.log("The perimeter of a rectangle of dimensions length = "
                   + l + " and breadth = " + b + " is " + rectangle.perimeter());
-	}
+	    }
     });
 };
 
