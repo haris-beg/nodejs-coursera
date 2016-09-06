b4732e88ee2cb3f70ab56ec484e6e7ca0d7ac5fe
Haris Beg <haris.beg@gmail.com>
2016-09-06 16:51:10 -0700

    Fixed bug that was causing 404 return code
    Had forgotten to mount the routers for dishes, promotions, and
    leadership when I copied the router files in the last commit

--- node-express-gen/app.js
+++ node-express-gen/app.js
@@ -27,6 +27,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/dishes',dishRouter);
+app.use('/promotions',promoRouter);
+app.use('/leadership',leaderRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
